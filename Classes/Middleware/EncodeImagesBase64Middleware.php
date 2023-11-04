996a396906c45aaac33c4473971c3c0c66278c6a
Maik Schneider <mas@xima.de>
2023-11-04 14:27:39 +0100

    feat: resolve svg sprites to actual icon markup

--- Classes/Middleware/EncodeImagesBase64Middleware.php
+++ Classes/Middleware/EncodeImagesBase64Middleware.php
@@ -2,17 +2,25 @@
 
 namespace Xima\XimaTypo3Manual\Middleware;
 
+use DOMDocument;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use SimpleXMLElement;
 use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Http\NullResponse;
 use TYPO3\CMS\Core\Http\Stream;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 
 class EncodeImagesBase64Middleware implements MiddlewareInterface
 {
+    /**
+     * array<string, string>
+     */
+    protected array $svgSpriteCache = [];
+
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $response = $handler->handle($request);
@@ -36,6 +44,30 @@ class EncodeImagesBase64Middleware implements MiddlewareInterface
         return $response;
     }
 
+    protected function parseImageUrlsAndEncodeBase64(string $input = ''): string
+    {
+        $pattern = '/<img[^>]+src="([^">]+)"/';
+
+        return preg_replace_callback($pattern, static function ($img): string {
+            if (!is_array($img)) {
+                return '';
+            }
+            $path = Environment::getPublicPath();
+            $path .= str_starts_with($img[1], '/') ? $img[1] : '/' . $img[1];
+            if (!file_exists($path)) {
+                return $img[0];
+            }
+            $fileContent = GeneralUtility::getUrl($path);
+            if (!$fileContent) {
+                return $img[0];
+            }
+
+            $fileType = mime_content_type($path);
+            $newSrc = 'data:' . $fileType . ';base64,' . base64_encode($fileContent);
+            return str_replace($img[1], $newSrc, $img[0]);
+        }, $input) ?? '';
+    }
+
     protected function convertSvgsToImage(string $input = ''): string
     {
         $doc = new \DOMDocument();
@@ -45,6 +77,7 @@ class EncodeImagesBase64Middleware implements MiddlewareInterface
         /** @var \DOMNode $svg */
         foreach ($svgs as $svg) {
             $svgMarkup = $svg->ownerDocument->saveHTML($svg);
+            $this->resolveSvgSprite($svgMarkup);
             $img = $doc->createElement('img', '');
             $img->setAttribute('src', 'data:image/svg+xml;base64,' . base64_encode($svgMarkup));
             $svg->parentNode->insertBefore($img);
@@ -52,28 +85,39 @@ class EncodeImagesBase64Middleware implements MiddlewareInterface
         return $doc->saveHTML();
     }
 
-    protected function parseImageUrlsAndEncodeBase64(string $input = ''): string
+    protected function resolveSvgSprite(string &$svgMarkup): void
     {
-        $pattern = '/<img[^>]+src="([^">]+)"/';
+        $pattern = '/<use[^>]+xlink:href="([^">]+)"/';
+        if (!preg_match_all($pattern, $svgMarkup, $svgParts)) {
+            return;
+        }
 
-        return preg_replace_callback($pattern, static function ($img): string {
-            /* @phpstan-ignore-next-line */
-            if (!is_array($img)) {
-                return '';
-            }
-            $path = Environment::getPublicPath();
-            $path .= str_starts_with($img[1], '/') ? $img[1] : '/' . $img[1];
-            if (!file_exists($path)) {
-                return $img[0];
-            }
-            $fileContent = file_get_contents($path);
-            if (!$fileContent) {
-                return $img[0];
-            }
+        $urlParts = GeneralUtility::trimExplode('#', $svgParts[1][0] ?? '');
+        if (count($urlParts) !== 2) {
+            return;
+        }
 
-            $fileType = mime_content_type($path);
-            $newSrc = 'data:' . $fileType . ';base64,' . base64_encode($fileContent);
-            return str_replace($img[1], $newSrc, $img[0]);
-        }, $input) ?? '';
+        // resolve sprite url, cache requests
+        $spriteUrl = Environment::getPublicPath() . '/' . $urlParts[0];
+        $spriteUrlHash = md5($spriteUrl);
+        $spriteMarkup = $this->svgSpriteCache[$spriteUrlHash] ?? GeneralUtility::getUrl($spriteUrl);
+        $this->svgSpriteCache[$spriteUrlHash] = $spriteMarkup;
+        if (!$spriteMarkup) {
+            return;
+        }
+
+        // parse sprite content
+        $svg = new SimpleXMLElement($spriteMarkup);
+        $namespaces = $svg->getDocNamespaces() ?: [''];
+        $svg->registerXPathNamespace('__nons', $namespaces['']);
+        $searchResult = $svg->xpath('//__nons:symbol[@id="' . $urlParts[1] . '"]');
+        if (!isset($searchResult[0]) || !$searchResult[0] instanceof SimpleXMLElement) {
+            return;
+        }
+
+        $iconMarkup = $searchResult[0]->asXML();
+        if (is_string($iconMarkup)) {
+            $svgMarkup = $iconMarkup;
+        }
     }
 }
