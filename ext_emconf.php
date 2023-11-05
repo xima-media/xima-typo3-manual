d8786b234b5a35e2965513b717f0f2812bf59826
Maik Schneider <mas@xima.de>
2023-11-05 19:12:17 +0100

    feat: add extension icon, ext_emconf.php, update composer.json

---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+
+/** @var string $_EXTKEY */
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Editor manual for TYPO3 backend',
+    'description' => 'This extension provides a new page type for creating an editor manual right in the TYPO3 backend.',
+    'category' => 'module',
+    'author' => 'Maik Schneider',
+    'author_email' => 'maik.schneider@xima.de',
+    'author_company' => 'XIMA Media GmbH',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'php' => '8.2.0-8.99.99',
+            'typo3' => '12.0.0-12.99.99',
+            'bw_focuspoint_images' => '4.0.0-4.99.99',
+            'bw_icons' => '3.0.0-3.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
