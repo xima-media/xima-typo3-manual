dc36992332c542fa41a4942dca46e0a4a4f3b268
Maik Schneider <mas@xima.de>
2024-03-23 19:55:44 +0100

    feat: add dropdown button for multiple manual links

--- Classes/EventListener/ModifyButtonBarEventListener.php
+++ Classes/EventListener/ModifyButtonBarEventListener.php
@@ -4,9 +4,12 @@ namespace Xima\XimaTypo3Manual\EventListener;
 
 use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Backend\Routing\UriBuilder;
+use TYPO3\CMS\Backend\Template\Components\Buttons\DropDown\DropDownItem;
 use TYPO3\CMS\Backend\Template\Components\ModifyButtonBarEvent;
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 final readonly class ModifyButtonBarEventListener
 {
@@ -20,23 +23,97 @@ final readonly class ModifyButtonBarEventListener
         $request = $GLOBALS['TYPO3_REQUEST'];
         $pageId = (int)($request->getParsedBody()['id'] ?? $request->getQueryParams()['id'] ?? 0);
 
-        if (!$pageId) {
+        $uri = $request->getUri()->getPath();
+        if (str_contains($uri, 'help/manual') || (!$pageId && !str_contains($uri, 'record/edit'))) {
             return;
         }
 
-        $uri = $request->getUri();
-        if (str_contains($uri, 'help/manual')) {
-            return;
+        $manualPages = [];
+
+        // edit record view
+        if (str_contains($uri, 'record/edit')) {
+            $editParam = $request->getQueryParams()['edit'] ?? '';
+            $recordTable = array_key_first($editParam);
+            $recordUid = (int)array_key_first($editParam[$recordTable] ?? []);
+            $recordType = $GLOBALS['TCA'][$recordTable]['ctrl']['type'] ?? '0';
+            array_push($manualPages, ...$this->getManualElementsForRecord($recordUid, $recordTable, $recordType));
+            array_push($manualPages, ...$this->getManualPagesForRecord($recordUid, $recordTable, $recordType));
         }
 
-        $manualButton = $event->getButtonBar()->makeLinkButton();
-        $manualButton->setHref($this->uriBuilder->buildUriFromRoute('xima_typo3_manual', ['id' => $pageId]));
-        $manualButton->setTitle('Manual');
-        $manualButton->setIcon($this->iconFactory->getIcon('apps-pagetree-manual-root', Icon::SIZE_SMALL));
+        // page view, list view, etc.
+        if ($pageId) {
+            array_push($manualPages, ...$this->getManualPagesForRecord($pageId, 'pages', 'doktype'));
+        }
+
         $buttons = $event->getButtons();
         $buttons['right'] ??= [];
-        $buttons['right'][] = [$manualButton];
+
+        if (count($manualPages)) {
+            $dropdown = $event->getButtonBar()->makeDropDownButton();
+            $dropdown->setLabel('Manual');
+            $dropdown->setTitle('Open manual page');
+            $dropdown->setShowLabelText(true);
+            $dropdown->setIcon($this->iconFactory->getIcon('apps-pagetree-manual-root', Icon::SIZE_SMALL));
+            foreach ($manualPages as $manualPage) {
+                // manual page
+                if (isset($manualPage['title'])) {
+                    $dropdown->addItem(
+                        GeneralUtility::makeInstance(DropDownItem::class)
+                            ->setLabel($manualPage['title'])
+                            ->setHref($this->uriBuilder->buildUriFromRoute('xima_typo3_manual',
+                                ['id' => $manualPage['uid']]))
+                    );
+                }
+                // manual element
+                if (isset($manualPage['header'])) {
+                    $dropdown->addItem(
+                        GeneralUtility::makeInstance(DropDownItem::class)
+                            ->setLabel($manualPage['header'])
+                            ->setHref($this->uriBuilder->buildUriFromRoute('xima_typo3_manual',
+                                ['id' => $manualPage['pid']]))
+                    );
+                }
+            }
+            $buttons['right'][] = [$dropdown];
+        } else {
+            $manualButton = $event->getButtonBar()->makeLinkButton();
+            $manualButton->setHref($this->uriBuilder->buildUriFromRoute('xima_typo3_manual', ['id' => $pageId]));
+            $manualButton->setTitle('Manual');
+            $manualButton->setIcon($this->iconFactory->getIcon('apps-pagetree-manual-root', Icon::SIZE_SMALL));
+
+            $buttons['right'][] = [$manualButton];
+        }
 
         $event->setButtons($buttons);
+    }
+
+    protected function getManualElementsForRecord(int $recordUid, string $recordTable, string $recordType): array
+    {
+        $sql = sprintf('select c.uid, c.pid, c.header from %s r, tt_content c where r.uid=%s and FIND_IN_SET(concat("%s:", r.%s), (c.tx_ximatypo3manual_relations)) and c.deleted=0 and c.hidden=0',
+            $recordTable, $recordUid, $recordTable, $recordType);
+
+        $connection = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('tt_content');
+        $result = $connection->executeQuery($sql)->fetchAllAssociative();
+
+        if (!$result) {
+            return [];
+        }
+
+        return $result;
+    }
+
+    protected function getManualPagesForRecord(int $recordUid, string $recordTable, string $recordType): array
+    {
+        $sql = sprintf('select p.uid, p.title from %s r, pages p where r.uid=%s and FIND_IN_SET(concat("%s:", r.%s), (p.tx_ximatypo3manual_relations)) and p.deleted=0 and p.hidden=0',
+            $recordTable, $recordUid, $recordTable, $recordType);
+
+        $connection = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('pages');
+        $result = $connection->executeQuery($sql)->fetchAllAssociative();
+
+        if (!$result) {
+            return [];
+        }
+
+        return $result;
     }
 }
