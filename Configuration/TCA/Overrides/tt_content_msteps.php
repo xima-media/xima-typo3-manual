b323129243e6c851f1cd2863f44ea3b6613a3e23
ctr <ctr@xima.de>
2024-05-03 13:34:13 +0200

    feat: added steps element

---
+++ Configuration/TCA/Overrides/tt_content_msteps.php
@@ -0,0 +1,38 @@
+<?php
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use Xima\XimaTypo3Manual\Preview\MstepsPreviewRenderer;
+
+ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'Standard',
+        'msteps',
+        'content-carousel-item-textandimage',
+    ],
+    'image',
+    'after'
+);
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['msteps'] = 'content-carousel-item-textandimage';
+
+$GLOBALS['TCA']['tt_content']['palettes']['msteps'] = [
+    'label' => 'LLL:EXT:xima_typo3_manual/Resources/Private/Language/locallang.xlf:msteps.palette',
+    'showitem' => 'header,--linebreak--,tx_ximatypo3manual_children',
+];
+
+$GLOBALS['TCA']['tt_content']['types']['msteps'] = [
+    'showitem' => '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                    --palette--;;msteps,
+                --div--;LLL:EXT:xima_typo3_manual/Resources/Private/Language/locallang.xlf:tab.manual_relations,
+                    --palette--;;manual-relations,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                    --palette--;;language,colPos',
+    'columnsOverrides' => [
+        'tx_ximatypo3manual_children' => [
+            'label' => 'Slides',
+        ],
+    ],
+];
+
+$GLOBALS['TCA']['tt_content']['types']['msteps']['previewRenderer'] = MstepsPreviewRenderer::class;
